9fa083e102478a72ebbce63ee61603a077a18e5b
artem1101gavrilov <artem1101gavrilov@gmail.com>
2018-07-12 09:56:29 +0300

    Сохранение листов классов.

--- SerializableSaveLoadData/Program.cs
+++ SerializableSaveLoadData/Program.cs
@@ -9,6 +9,26 @@ namespace ConsoleApplication1
 {
     class Program
     {
+        public class ItemData2
+        {
+            public int id;
+            public string name;
+            public bool isStackable;
+        }
+
+        [System.Serializable]
+        public class ItemData
+        {
+            public int id;
+            public string name;
+            public bool isStackable;
+        }
+        [System.Serializable]
+        public class Quest
+        {
+            public int id; //Номер квеста
+            public string name; //Текстовое поле для кнопки
+        }
         [System.Serializable]
         public class ClassSave //Класс для сохранения данных персонажа
         {
@@ -16,11 +36,31 @@ namespace ConsoleApplication1
             public bool bool1;
             public string stroka;
             public List<int> ListInt;
+            public List<ItemData> ListItem;
+            public List<Quest> ListQuest;
         }
 
         static void SaveData()
         {
+            ItemData2 item = new ItemData2();
+            item.id = 4;
+            item.name = "dfg";
+            item.isStackable = true;
+
+            ItemData item0 = new ItemData();
+            item0.id = item.id;
+            item0.isStackable = item.isStackable;
+            item0.name = item.name;
+
+            Quest quest = new Quest();
+            quest.id = 0;
+            quest.name = "45fgh";
+
             ClassSave Save1 = new ClassSave();
+            Save1.ListItem = new List<ItemData>();
+            Save1.ListItem.Add(item0);
+            Save1.ListQuest = new List<Quest>();
+            Save1.ListQuest.Add(quest);
             Save1.ListInt = new List<int>();
             Save1.ListInt.Add(4);
             Save1.ListInt.Add(2);
@@ -46,6 +86,8 @@ namespace ConsoleApplication1
                 {
                     ClassSave Load1 = (ClassSave)bformatter.Deserialize(fs);
                     Console.Write(Load1.stroka + Load1.ListInt[3]);
+                    Console.Write(Load1.ListItem[0].id + Load1.ListItem[0].name + Load1.ListItem[0].isStackable);
+                    Console.Write(Load1.ListQuest[0].id + Load1.ListQuest[0].name);
                 }
                 catch (System.Exception e)
                 {
